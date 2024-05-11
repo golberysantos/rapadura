32420587d244ba5830fb1eef04dea0bc4c0ef81d
Golbery <golbery.santos@yahoo.com.br>
2024-05-11 11:41:37 -0400

    teste na tabela usuario, contato e tipo contato

--- db/GOLBERY-SQL-TESTE-UNITARIO-DB.sql
+++ db/GOLBERY-SQL-TESTE-UNITARIO-DB.sql
@@ -1,14 +1,96 @@
-
 -- -----------------------
--- TESTE UNITÁRIO
+-- GOLBERY. RAPADURA TESTE UNITÁRIO DB
 -- -----------------------
+USE rapadura;
 
--- CADA. PESSOA
-INSERT INTO PESSOA (id) VALUE (1);
+-- PESSOA
+INSERT INTO PESSOA 
+	(observacao) 
+VALUE 
+	('TESTE'), 
+    ('TESTE'),
+    ('TESTE'),
+    ('TESTE'),
+    ('TESTE')   
+;
+
 select * from pessoa;
 
+update pessoa
+set observacao = 'TESTE PES.1'
+where id = 1
+;
+
+update pessoa
+set observacao = 'TESTE PES.2'
+where id = 2
+;
+
+update pessoa
+set observacao = 'TESTE PES.3'
+where id = 3
+;
+
+update pessoa
+set observacao = 'TESTE PES.4'
+where id = 4
+;
+
+update pessoa
+set observacao = 'TESTE PES.5'
+where id = 5
+;
+
+delete 
+from pessoa
+where 
+	id = 5;
+;
+select * from pessoa;
+
+
+-- TIPOCONTATO
+insert into tipocontato
+	(nome)
+value
+	('WPP'), 
+    ('CELULAR'), 
+    ('E-MAIL'),
+    ('SITE'),
+    ('INSTAGRAM'),
+    ('LINKEDIN'),
+    ('TELEGRAM'),
+    ('X'),
+    ('FACEBOOK'),
+    ('COMERCIAL')
+;
+
+-- CONTATO
+insert into contato
+	(valor, pessoa_id, tipo_contato_id)
+value
+	('ps1@ps.com', 1, 3)
+;
+
+insert into contato
+	(valor, pessoa_id, tipo_contato_id)
+value
+	('@ps', 2, 5),
+    ('www.ps.com', 3, 4),
+    ('9211111111', 4, 1)
+;
+select * from contato order by id;
+delete from contato where id = 4; 
+
+
+
+
+
+
+
+/*
 -- CADA. USUARIO
-USE rapadura;
+
 -- FALTA ATUALIZAR: (id) AI para, (senha) tipo varchar(255),  
 INSERT INTO usuario 
 		(idusuario, senha, login, nickname, RI, pessoa_id)
@@ -17,7 +99,7 @@ INSERT INTO usuario
 	;
 select * from usuario;
 
-
+*/
 
 
 
